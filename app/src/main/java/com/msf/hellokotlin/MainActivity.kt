99763e66be584df385a2111002e431dd80d9a757
Karthick <karthick.m@marketsimplified.com>
2017-10-07 17:07:03 +0530

    button click listener added

--- app/src/main/java/com/msf/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/msf/hellokotlin/MainActivity.kt
@@ -5,7 +5,11 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
+/**
+ * This class has Basic concepts of Kotlin - Functions, Variables, String templates
+ */
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
 
@@ -14,22 +18,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         toast("Hi")
         multiply(2)
-        variables()
 
         val textView = findViewById<TextView>(R.id.textView)
         textView.text = "Hello Kotlin"
         toast1("Hey ${textView.text}")
 
+        /*Button click listener to show toast message*/
+        button.setOnClickListener({ toast("You typed, ${message.text}") })
 
     }
 
+
+    /**
+     *To show toast message in compact style ;)
+     */
     private fun toast1(message: String) = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 
+    /**
+     * To show a toast message
+     */
     private fun toast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
+    /**
+     * A function with return type of Int in a compact format
+     */
     private fun multiply(myInt: Int) = Log.i(TAG, (myInt * 2).toString())
+
 
     private fun variables() {
         val x: Int = 20
